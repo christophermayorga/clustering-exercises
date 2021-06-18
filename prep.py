1e2b76848428f36137fde8ecb94d147bf8cbfa38
Christopher Mayorga <christopher.mayorga2@gmail.com>
2021-06-18 12:31:48 -0500

    Adds functions to prep

--- prep.py
+++ prep.py
@@ -52,9 +52,74 @@ def handle_outliers(df, col):
     df_out = df.loc[(df[col] > lower_bound) & (df[col] < upper_bound)]
     return df_out
 
-def handle_missing_values(df, prop_required_columns=0.5, prop_required_row=0.75):
-    threshold = int(round(prop_required_columns * len(df.index),0))
-    df = df.dropna(axis=1, thresh=threshold)
-    threshold = int(round(prop_required_row * len(df.columns),0))
-    df = df.dropna(axis=0, thresh=threshold)
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+    
+    for col in col_list:
+
+        q1, q3 = df[f'{col}'].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        return df[(df[f'{col}'] > lower_bound) & (df[f'{col}'] < upper_bound)]  
+    
+
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .5):
+    ''' 
+        take in a dataframe and a proportion for columns and rows
+        return dataframe with columns and rows not meeting proportions dropped
+    '''
+    col_thresh = int(round(prop_required_column*df.shape[0],0)) # calc column threshold
+    
+    df.dropna(axis=1, thresh=col_thresh, inplace=True) # drop columns with non-nulls less than threshold
+    
+    row_thresh = int(round(prop_required_row*df.shape[1],0))  # calc row threshhold
+    
+    df.dropna(axis=0, thresh=row_thresh, inplace=True) # drop columns with non-nulls less than threshold
+    
+    return df    
+    
+    
+def impute(df, my_strategy, column_list):
+    ''' take in a df strategy and cloumn list
+        return df with listed columns imputed using input stratagy
+    '''
+        
+    imputer = SimpleImputer(strategy=my_strategy)  # build imputer
+
+    df[column_list] = imputer.fit_transform(df[column_list]) # fit/transform selected columns
+
+    return df
+
+def prepare_zillow(df):
+    ''' Prepare Zillow Data'''
+    
+    # Restrict propertylandusedesc to those of single unit
+    df = df[(df.propertylandusedesc == 'Single Family Residential') |
+          (df.propertylandusedesc == 'Mobile Home') |
+          (df.propertylandusedesc == 'Manufactured, Modular, Prefabricated Homes') |
+          (df.propertylandusedesc == 'Townhouse')]
+    
+    # remove outliers in bed count, bath count, and area to better target single unit properties
+    df = remove_outliers(df, 1.5, ['calculatedfinishedsquarefeet', 'bedroomcnt', 'bathroomcnt'])
+    
+    # dropping cols/rows where more than half of the values are null
+    df = handle_missing_values(df, prop_required_column = .5, prop_required_row = .5)
+    
+    # dropping the columns with 17K missing values too much to fill/impute/drop rows
+    df = df.drop(columns=['heatingorsystemtypeid', 'buildingqualitytypeid', 'propertyzoningdesc', 'unitcnt', 'heatingorsystemdesc'])
+    
+    # imputing descreet columns with most frequent value
+    df = impute(df, 'most_frequent', ['calculatedbathnbr', 'fullbathcnt', 'regionidcity', 'regionidzip', 'yearbuilt', 'censustractandblock'])
+    
+    # imputing continuous columns with median value
+    df = impute(df, 'median', ['finishedsquarefeet12', 'lotsizesquarefeet', 'structuretaxvaluedollarcnt', 'taxvaluedollarcnt', 'landtaxvaluedollarcnt', 'taxamount'])
+    
     return df
